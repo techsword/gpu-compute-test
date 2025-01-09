71851f57f6d0ca5312cb4f79fb888a33118eb160
techsword <gaofei.shen@techsword.me>
2025-01-09 22:12:05 +0100

    tested pytorch-xpu with wav2vec2 model, added required python packages

---
+++ intel-test.py
@@ -0,0 +1,32 @@
+import os 
+import glob
+
+from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
+from datasets import load_dataset
+import torch
+
+device = 'xpu' if torch.xpu.is_available() else 'cpu'
+print(f"Using {device} device")
+
+# load model and tokenizer
+processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
+model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+model.eval()
+model.to(device)
+    
+# load dummy dataset and read soundfiles
+ds = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
+
+
+for i in range(len(ds)):
+    # tokenize
+    input_values = processor(ds[i]["audio"]["array"], return_tensors="pt", padding="longest", sampling_rate=16000).input_values  # Batch size 1
+
+    # retrieve logits
+    logits = model(input_values.to(device)).logits
+
+    # take argmax and decode
+    predicted_ids = torch.argmax(logits, dim=-1)
+
+    transcription = processor.batch_decode(predicted_ids)
+    print(transcription)
